1d88118363f511097724264a03b64f8a7fae93de
tamBug <nhutam181103@gmail.com>
2025-03-06 12:53:32 +0700

    fix: repositories folder

--- SWD392-AffiliLinker.Repositories/DependencyInjection.cs
+++ SWD392-AffiliLinker.Repositories/DependencyInjection.cs
@@ -2,13 +2,14 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SWD392_AffiliLinker.Repositories.Context;
+using SWD392_AffiliLinker.Repositories.Interfaces;
 using SWD392_AffiliLinker.Repositories.IUOW;
 using SWD392_AffiliLinker.Repositories.Repositories;
 using SWD392_AffiliLinker.Repositories.UOW;
 
 namespace SWD392_AffiliLinker.Repositories
 {
-	public static class DependencyInjection
+    public static class DependencyInjection
 	{
 		public static IServiceCollection AddRepositoryConfig(this IServiceCollection services)
 		{
--- SWD392-AffiliLinker.Repositories/UOW/IUnitOfWork.cs
+++ SWD392-AffiliLinker.Repositories/UOW/IUnitOfWork.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SWD392_AffiliLinker.Repositories.Interfaces;
 
 namespace SWD392_AffiliLinker.Repositories.IUOW
 {
