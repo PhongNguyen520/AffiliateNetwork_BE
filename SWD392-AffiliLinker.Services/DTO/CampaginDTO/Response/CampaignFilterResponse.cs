11fbb23174a70c530dfb253a4d4c224525373e5f
PhongNguyen <PhongNTSE173175@fpt.edu.vn>
2025-03-27 13:23:04 +0700

    Connnect DB

--- SWD392-AffiliLinker.Services/DTO/CampaginDTO/Response/CampaignFilterResponse.cs
+++ SWD392-AffiliLinker.Services/DTO/CampaginDTO/Response/CampaignFilterResponse.cs
@@ -15,5 +15,6 @@ namespace SWD392_AffiliLinker.Services.DTO.CampaginDTO.Response
 		public decimal ConversionRate { get; set; }
 		public DateTime StartDate { get; set; }
 		public DateTime EndDate { get; set; }
+		public string Status { get; set; }
 	}
 }
